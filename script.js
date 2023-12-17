fe06ab2284dd3118b5c512f1608ce1443eb70e60
swed <kamil.it.projects@gmail.com>
2023-12-17 09:10:18 +0100

    niedziela

---
+++ script.js
@@ -0,0 +1,16 @@
+function guess() {
+    // funkcja pobiera pojedynczy znak z pola tekstowego na stronie i wyszukuje oraz ujawnia pasujące znaki w haśle
+
+
+    // zgadywany znak
+    let guessChar = document.getElementById("guessedChar").value;
+    // po pobraniu znaku czyścimy pole tekstowe
+    document.getElementById("guessedChar").value = ""
+
+    console.log("Próba odgadnięcia znaku: " + guessChar);
+}
+
+window.addEventListener("load", () => ) {
+
+    document.getElementById("guessed")
+}
